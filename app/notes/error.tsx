790b68f6fe587b9828c95f3dda4157273c16bad0
Svitlana <svetagimish@gmail.com>
2026-05-11 23:33:08 +0300

    error.tsx

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -1,12 +1,10 @@
 "use client";
+
 type Props = {
   error: Error;
 };
+
 export default function Error({ error }: Props) {
-  return (
-    <p>
-      Could not fetch the list of notes.
-      {error.message}
-    </p>
-  );
+  console.log(error);
+  return <p>Could not fetch the list of notes. {error.message}</p>;
 }
