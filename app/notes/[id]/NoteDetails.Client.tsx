a4f7b52d35dacb519741bdfc9af5bfcf6781e6ea
Svitlana <svetagimish@gmail.com>
2026-05-11 22:54:59 +0300

    fix

--- app/notes/[id]/NoteDetails.Client.tsx
+++ app/notes/[id]/NoteDetails.Client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import css from "./NoteDetails.Client.module.css";
+import css from "./NoteDetails.client.module.css";
 import { useParams } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
 
