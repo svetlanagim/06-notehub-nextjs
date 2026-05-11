5144a9373f6d1b1dbe7ef13fcbf5f00f2ec0eadd
Svitlana <svetagimish@gmail.com>
2026-05-11 22:41:48 +0300

    .

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetailsClient";
+import NoteDetailsClient from "./NoteDetails.client";
 
 export default async function NoteDetailsPage({
   params,
