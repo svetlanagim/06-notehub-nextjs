e7ca3a263ec7c391e9440b824ad03ecfce761048
Svitlana <svetagimish@gmail.com>
2026-05-11 23:07:00 +0300

    hw6

--- app/notes/[id]/NoteDetails.Client.tsx
+++ app/notes/[id]/NoteDetails.Client.tsx
@@ -2,7 +2,9 @@
 
 import { useQuery } from "@tanstack/react-query";
 import css from "./NoteDetails.client.module.css";
+
 import { useParams } from "next/navigation";
+
 import { fetchNoteById } from "@/lib/api";
 
 type Params = {
